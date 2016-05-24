d144850f2255da69a22ad20aa6773b6bd4cdb481
Michael Danko <MichaelDanko@icloud.com>
2016-05-24 12:15:25 -0400

    Sized the header and have a gradient in it

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,12 @@ var sass = require('gulp-sass');
 gulp.task('default', ['css']);
 
 gulp.task('css', function () {
-    return gulp.src('./scss/*.scss')
+    return gulp.src('./*.scss')
         .pipe(sass())
         .pipe(gulp.dest('./css'));
 });
 
 gulp.task('watch', function () {
-    gulp.watch('./scss/*.scss', ['css']);
+    gulp.watch('./*.scss', ['css']);
 });
 
